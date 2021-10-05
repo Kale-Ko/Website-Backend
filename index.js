3aba1a9e21f3c65b599ab104fde7728988c256f4
Kale-Ko <github@kaleko.ga>
2021-10-05 11:40:46 -0400

    Update

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ async function handelRequest(req) {
                                 var todelete = ["id", "node_id", "private", "owner", "fork", "html_url", "forks_url", "keys_url", "collaborators_url", "teams_url", "hooks_url", "issue_events_url", "events_url", "assignees_url", "branches_url", "tags_url", "blobs_url", "git_tags_url", "git_refs_url", "trees_url", "statuses_url", "languages_url", "stargazers_url", "contributors_url", "subscribers_url", "subscription_url", "commits_url", "git_commits_url", "comments_url", "issue_comment_url", "contents_url", "compare_url", "merges_url", "archive_url", "downloads_url", "issues_url", "pulls_url", "milestones_url", "notifications_url", "labels_url", "releases_url", "deployments_url", "pushed_at", "git_url", "ssh_url", "clone_url", "svn_url", "size", "has_issues", "has_projects", "has_downloads", "has_wiki", "has_pages", "forks_count", "mirror_url", "open_issues_count", "license", "allow_forking", "forks", "open_issues", "watchers", "default_branch", "permissions", "language", "disabled", "created_at"]
                                 todelete.forEach(item => { delete repo[item] })
 
-                                showcase.push(repo)
+                                showcase[pinned.indexOf(repo.name)] = repo
                             }
                         })
                     })
@@ -103,7 +103,7 @@ async function handelRequest(req) {
 
                     await fetch("https://api.github.com/users/" + CONFIG.GITHUB_USERNAME + "/repos?per_page=100", { headers: { "User-Agent": "Mozilla/5.0 Cloudflare/Workers", "Authorization": "token ghp_" + CONFIG.GITHUB_API_TOKEN } }).then(res => res.json()).then(data => {
                         data.forEach(repo => {
-                            if (pinned.includes(repo.name) && !repo.private) showcase.push(repo)
+                            if (pinned.includes(repo.name) && !repo.private) showcase[pinned.indexOf(repo.name)] = repo
                         })
                     })
                 })
